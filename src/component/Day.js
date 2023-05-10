f8b6ea028217ff057de8fe5d1f8a3b21d079ab6d
restvoice2020@gmail.com <restvoice2020@gmail.com>
2023-05-10 17:40:01 +0900

    230510

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,21 +1,34 @@
 import { useParams } from "react-router-dom"
-import dummy from "../db/data.json"
 import Word from "./Word";
+import { useEffect, useState } from "react";
+import useFetch from "../hooks/useFetch";
 
 export default function Day (){
-
+    
     // const inputDay = 3;
     const {inputDay} = useParams();
-    const wordList = dummy.words.filter(word => (word.day === Number(inputDay)))
+    
+    // const [words, setWords] = useState([])
+
+    // useEffect(() => {
+    //     fetch(`http://localhost:4000/words?day=${inputDay}`) // words만 입력하면 전체를 가져오기때문에 이렇게 조회 조건을 걸어준다.
+    //     .then(res => {return res.json();})
+    //     .then(data => {setWords(data);})
+    // },[inputDay])
+
+    // const wordList = words.filter(word => (word.day === Number(inputDay)))
     // wordList는 filter 함수로 day가 3인 요소들을 담은 배열집합
     // words 배열집합을 filter로 재가공한 또 다른 배열집합
     // useParams는 숫자여도 문자(string)로 인식하기 때문에 inputDay를 숫자(Number)타입으로 바꿔줄 필요가 있다.(url 주소에 들어간 숫자는 문자로 인식!) 
+
+    const words = useFetch(`http://localhost:4000/words?day=${inputDay}`)
+
     return(
         <>
             <h2>오늘의 학습 : Day {inputDay}</h2>
                 <table>
                     <tbody>
-                        {wordList.map(word => (
+                        {words.map(word => (
                             <Word word={word} key={word.id}/>
                         ))}
                     </tbody>
--- src/component/DayList.js
+++ src/component/DayList.js
@@ -1,11 +1,23 @@
-import { Link } from "react-router-dom"
-import dummy from "../db/data.json"
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import useFetch from "../hooks/useFetch";
 
 export default function DayList (){
+    // const [days, setDays] = useState([])
+
+    // // useEffect(함수, [변화할 변수 또는 조건]) --> 빈 배열로 넣으면 초기 화면 랜더링 이후 한번만 실행!
+    // // 즉, 랜더링 이후, json 데이터를 한번 가져옴
+    // useEffect(() => {
+    //     fetch("http://localhost:4000/days") // 데이터 연결하려는 url 주소
+    //     .then(res => {return res.json();}) // url 주소상에 있는 데이터를 json형태로 바꿔준다(key-value 형태)
+    //     .then(data => {setDays(data);}) // data로 상태 관리할 데이터를 넣어준다
+    // },[])
+
+    const days = useFetch("http://localhost:4000/days")
 
     return(
         <ul className="list_day">
-            {dummy.days.map(day => (
+            {days.map(day => (
                 <li key={day.id}>
                     <Link to={`/day/${day.day}`}>
                         Day {day.day}
