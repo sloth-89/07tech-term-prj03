ca7c3527799f635342920a153cfbd178ebed2964
restvoice2020@gmail.com <restvoice2020@gmail.com>
2023-05-16 17:11:44 +0900

    230516

--- src/component/Word.js
+++ src/component/Word.js
@@ -1,22 +1,48 @@
 import { useState } from "react";
 
-export default function Word ({word}){
+export default function Word ({word: w}){
 
+    const [word, setWord] = useState(w)
     const [isShow, setIsShow] = useState(false);
-    const toggleShow = () => {
+    const [isDone, setIsDone] = useState(word.isDone);
+
+    function toggleShow(){
         setIsShow(!isShow);
+    }
+
+    function toggleDone(){
+        setIsDone(!isDone)
+    }
+
+    function del(){
+        if(window.confirm("이 단어를 삭제 하시겠습니까?")){
+            fetch(`http://localhost:4000/words/${word.id}`,{
+                method: "DELETE",
+            }).then(res => {
+                if(res.ok){
+                    setWord({
+                        ...word,
+                        id: 0
+                    })
+                }
+            })
+        }
+    }
+
+    if(word.id === 0){
+        return null;
     }
     
     return(
-        <tr>
-            <td><input type="checkbox"/></td>
+        <tr className={isDone? "off" : ""}>
+            <td><input type="checkbox" checked={isDone} onChange={toggleDone}/></td>
             <td>{word.eng}</td>
             <td>{isShow && word.kor}</td>
             <td>
                 <button onClick={toggleShow}>뜻 {isShow ? "숨김":"보기"}</button>
             </td>
             <td>
-                <button className="btn_del">삭제</button>
+                <button className="btn_del" onClick={del}>삭제</button>
             </td>
         </tr>
     )
